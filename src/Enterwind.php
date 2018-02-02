7e0124906a4cd1cfaee58a5f4a74a0aa6a39b176
Noviyanto Rahmadi <noviyantorahmadi@gmail.com>
2018-02-02 13:47:11 +0800

    Little Bit Change

--- src/Enterwind.php
+++ src/Enterwind.php
@@ -6,14 +6,18 @@ use GuzzleHttp\Client as HttpClient;
 
 class Enterwind
 {
-	function __construct()
+    function __construct()
     {
-    	// 
+        // 
     }
 
     public function httpClient($method, $url, $custom = false)
     {
-    	$client = new HttpClient;
+        if($custom == true) {
+            $url = config('api-samarinda.api_url') . '/' . config('api-samarinda.api_version');
+        }
+
+        $client = new HttpClient;
         if($custom == true) {
             $res = $client->request($method, $url, [
                 'headers' => [
@@ -22,24 +26,15 @@ class Enterwind
                     'Accept' => 'application/json'
                 ]
             ]);
-        } else {
-            $namespace = config('api-samarinda.api_url') . '/' . config('api-samarinda.api_version');
-            $res = $client->request($method, $namespace . $url, [
-                'headers' => [
-                    'Authorization' => 'Bearer ' . config('api-samarinda.token'),
-                    'Content-Type'  => 'application/json',
-                    'Accept' => 'application/json'
-                ]
-            ]);
         }
+
         $res->getHeader('content-type');
+
         return response()->json(json_decode($res->getBody(), true));
     }
 
     public function forgetParams() 
     {
-        return response()->json([
-            'message' => 'Anda kurang parameters.'
-        ]);
+        return response()->json(['message' => 'Anda kurang parameters.']);
     }
 }
